05859e3faab2de7271741731c5a45ed950e9ab10
nanapap1 <markmoskvitin@MacBook-Pro-Mark.local>
2025-11-14 03:46:34 +0300

    Test backend

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,25 @@
 import { createRoot } from 'react-dom/client';
-import { MaxUI } from '@maxhub/max-ui';
+import {Button, MaxUI} from '@maxhub/max-ui';
 import '@maxhub/max-ui/dist/styles.css';
 import App from './App.jsx';
+import {useEffect} from "react";
 
 
-const webs = window.WebApp.initData;
-console.log(webs);
+const webs = window.WebApp;
+
 // eslint-disable-next-line react-refresh/only-export-components
 function Root() {
+    useEffect(() => {
+        webs.ready();
+    })
+
+
+    const onClose = () => {
+         webs.close();
+    }
+
     return (<MaxUI>
-        <App />
+        <Button onClick={onClose} />
     </MaxUI>)
     }
 createRoot(document.getElementById('root')).render(<Root />);
